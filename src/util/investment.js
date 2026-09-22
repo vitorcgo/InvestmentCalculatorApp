d5ecb392070b07716fa700df3101b986937e0265
Vitor <zbro2018@gmail.com>
2026-09-22 19:53:31 -0300

    docs: inspect investment calculation utility

--- src/util/investment.js
+++ src/util/investment.js
@@ -1,9 +1,4 @@
-// This function expects a JS object as an argument
-// The object should contain the following properties
-// - initialInvestment: The initial investment amount
-// - annualInvestment: The amount invested every year
-// - expectedReturn: The expected (annual) rate of return
-// - duration: The investment duration (time frame)
+// Recebe os dados de entrada para simular a evolução anual do investimento.
 export function calculateInvestmentResults({
   initialInvestment,
   annualInvestment,
@@ -16,20 +11,19 @@ export function calculateInvestmentResults({
   for (let i = 0; i < duration; i++) {
     const interestEarnedInYear = investmentValue * (expectedReturn / 100);
     investmentValue += interestEarnedInYear + annualInvestment;
+
     annualData.push({
-      year: i + 1, // year identifier
-      interest: interestEarnedInYear, // the amount of interest earned in this year
-      valueEndOfYear: investmentValue, // investment value at end of year
-      annualInvestment: annualInvestment, // investment added in this year
+      year: i + 1,
+      interest: interestEarnedInYear,
+      valueEndOfYear: investmentValue,
+      annualInvestment: annualInvestment,
     });
   }
 
   return annualData;
 }
 
-// The browser-provided Intl API is used to prepare a formatter object
-// This object offers a "format()" method that can be used to format numbers as currency
-// Example Usage: formatter.format(1000) => yields "$1,000"
+// A formatação da versão-base usa dólares dos EUA.
 export const formatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: 'USD',
